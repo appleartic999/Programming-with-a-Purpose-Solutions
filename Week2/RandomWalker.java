4afb69068b224802ddc1c5e9905c92395acb108f
Apple Artic <techapple652@gmail.com>
2023-02-23 19:26:40 +0530

    completed RandomWalker

--- Week2/RandomWalker.java
+++ Week2/RandomWalker.java
@@ -1,5 +1,26 @@
 public class RandomWalker {
     public static void main(String[] args) {
-
+        int r = Integer.parseInt(args[0]);
+        int cnt = 0;
+        int manhattanDistance = -1;
+//        Starting Co-ordinates
+        int x = 0;
+        int y = 0;
+        while (manhattanDistance != r) {
+            int randomDirection = (int) (Math.random() * 4);
+            if (randomDirection == 0) {
+                x++;
+            } else if (randomDirection == 1) {
+                y++;
+            } else if (randomDirection == 2) {
+                x--;
+            } else {
+                y--;
+            }
+            System.out.println("(" + x + " " + y + ")");
+            cnt++;
+            manhattanDistance = Math.abs(x) + Math.abs(y);
+        }
+        System.out.println("steps = " + cnt);
     }
 }
