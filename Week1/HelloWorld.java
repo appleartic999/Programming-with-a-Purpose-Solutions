7b5d76d20c3a4a18d952172d84005b37f4969919
Apple Artic <techapple652@gmail.com>
2023-02-23 17:07:42 +0530

    first commit

---
+++ Week1/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int n = 123456789;
+        int m = 0;
+        while (n != 0)
+        {
+            m = (10 * m) + (n % 10);
+            n = n / 10;
+        }
+        System.out.println(m);
+        int i,j;
+        for (i = 0, j = 0; i < 10; i++)
+            j += i;
+        System.out.println(j);
+    }
+}
